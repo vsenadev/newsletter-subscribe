f98b9e9e60a8c0ed3d54376c2f2f585e6d0c9825
Vinícius Sena <senavinicius01@gmail.com>
2023-12-08 22:56:11 -0300

    FEAT - html implement

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from '../src/pages/Home'
 
 export default function AppRouter() {
     return(
         <BrowserRouter>
             <Routes>
+                <Route element={<Home/>} path='/'/>
             </Routes>
         </BrowserRouter>
     )
